e2dadf20cf0ce5c75fec842129ddb10ab1414eb1
kblee <kblee@bonabank.com>
2018-11-21 12:22:26 +0900

    메세지박스 버튼 아이콘으로 수정

--- packages/message-box/src/main.vue
+++ packages/message-box/src/main.vue
@@ -50,6 +50,8 @@
         </div>
         <div class="el-message-box__btns">
           <el-button
+            icon="el-icon-close"
+            circle
             :loading="cancelButtonLoading"
             :class="[ cancelButtonClasses ]"
             v-if="showCancelButton"
@@ -57,9 +59,11 @@
             size="small"
             @click.native="handleAction('cancel')"
             @keydown.enter="handleAction('cancel')">
-            {{ cancelButtonText || t('el.messagebox.cancel') }}
+            <!--{{ cancelButtonText || t('el.messagebox.cancel') }}-->
           </el-button>
           <el-button
+            icon="el-icon-check"
+            circle
             :loading="confirmButtonLoading"
             ref="confirm"
             :class="[ confirmButtonClasses ]"
@@ -68,7 +72,7 @@
             size="small"
             @click.native="handleAction('confirm')"
             @keydown.enter="handleAction('confirm')">
-            {{ confirmButtonText || t('el.messagebox.confirm') }}
+            <!--{{ confirmButtonText || t('el.messagebox.confirm') }}-->
           </el-button>
         </div>
       </div>
